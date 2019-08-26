d4746e809ceee6a4d8d1d4537c7d463cd5e519b1
Eric Pardi <eric.pardi@gmail.com>
2019-08-26 00:36:02 -0400

    created users and posts before comments

--- database/migrations/2019_06_14_013728_create_comments_table.php
+++ database/migrations/2019_06_14_013728_create_comments_table.php
@@ -13,6 +13,33 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->unsignedBigInteger('post_id')->nullable();
+            // $table->foreign('post_id')->references('id')->on('posts')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('avatar')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image')->nullable();
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->string('author');
+            $table->longtext('body');
+            $table->datetime('published_at')->nullable();
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->nullable();
+            $table->timestamps();
+        });
+
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
@@ -36,5 +63,7 @@ class CreateCommentsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('comments');
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('users');
     }
 }
